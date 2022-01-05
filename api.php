b3d58c4b2ee0751fc41cc44fb93c019f4096e41e
Louis Ouellet <louis@TheMacSwitch.local>
2022-01-05 16:36:19 -0500

    22.01-dev-474

--- api.php
+++ api.php
@@ -2,119 +2,217 @@
 session_start();
 
 // Import API
-require_once dirname(__FILE__) . '/src/lib/api.php';
-
-// Start API
-$API = new API();
-
-// Load Plugin's API
-$API->loadFiles('api.php', 'api', 3);
+require_once dirname(__FILE__).'/src/lib/api.php';
 
 if(!empty($_POST)){
-	if(isset($_POST['data'])){
-		if($API->isJson($_POST['data'])){
-			$decodedJSON = json_decode($decodedURI, true);
-		} else {
-			$decodedBase64 = base64_decode($_POST['data']);
-			$decodedURI = urldecode($decodedBase64);
-			$decodedJSON = json_decode($decodedURI, true);
-		}
-	} else {
-		$decodedJSON = [];
-	}
-	if(isset($_POST['method'])){
-		switch($_POST['method']){
-			case "token":
-				if(isset($_POST['token'])){
-					$API->Auth->loginToken($_POST['token']);
-				} else {
-					echo "No Token Received";
-				}
-				break;
-			case "auth":
-				if(isset($_POST['username'],$_POST['password'])){
-					$API->Auth->login($_POST['username'],$_POST['password']);
-				} else {
-					echo "No Login Data Received";
-				}
-				break;
-			case "session":
-				// if(!empty($_SESSION)){ var_dump($_SESSION[$API->Settings['id']]); }
-				break;
-		}
-	} else {
-		$return = [
-			"error" => $API->Language->Field["unknown authentication method"],
-			"request" => $_POST,
-		];
-	}
-	if($API->Auth->isLogin()){
-		if((!isset($API->Settings['maintenance']))||(!$API->Settings['maintenance'])){
-			if((isset($_POST['request'],$_POST['type']))&&($_POST['request'] == 'api')&&($_POST['type'] == 'initialize')){
-				$return = $API->initApp();
-			} elseif((isset($_POST['request'],$_POST['type']))&&($_POST['request'] == 'api')&&($_POST['type'] == 'getLanguage')){
-				$return = $API->getLanguage();
-			} elseif((isset($_POST['request'],$_POST['type']))&&($_POST['request'] == 'smtp')&&($_POST['type'] == 'send')){
-				if(isset($decodedJSON['extra'])){$API->Auth->Mail->send($decodedJSON['email'],$decodedJSON['message'],$decodedJSON['extra']);}
-				else{$API->Auth->Mail->send($decodedJSON['email'],$decodedJSON['message']);}
-				$return = [
-					"success" => $API->Language->Field["Message sent"],
-					"request" => $_POST,
-				];
-			} else {
-				if((isset($_POST['request']))&&(class_exists($_POST['request'].'API'))){
-					$request = $_POST['request'].'API';
-					$request = new $request();
-					if((isset($_POST['type']))&&(method_exists($request,$_POST['type']))){
-						$return = $_POST['type'];
-						if(isset($decodedJSON)){ $return = $request->$return($_POST['request'], $decodedJSON); }
-						else { $return = $request->$return($_POST['request'], null); }
-					} else {
-						$return = [
-							"error" => $API->Language->Field["unknown request type"],
-							"request" => $_POST,
-						];
-					}
-				} else {
-					if((isset($_POST['request']))&&(!$API->Auth->valid('api',$_POST['request'],1))){
-						$return = [
-							"error" => $API->Language->Field["Insuffisant priviledges"],
-							"request" => $_POST,
-						];
-					} else {
-						$return = [
-							"error" => $API->Language->Field["unknown request"],
-							"request" => $_POST,
-						];
-					}
-				}
-			}
-		} else {
+	if(isset($_POST['request'])){
+		$trigger = $_POST['request'];
+		// Import API
+		$file = dirname(__FILE__).'/plugins/'.$trigger.'/'.'api.php';
+		if(is_file($file)){ require_once $file;$request = $trigger.'API'; }
+		else { $request = 'API'; }
+
+		// Start API
+		if(class_exists($request)){ $API = new $request(); }
+		else {
 			$return = [
-				"error" => $API->Language->Field["Server under maintenance"],
+				"error" => $API->Language->Field["Unknown API"],
 				"request" => $_POST,
-				"code" => 500,
+				"api" => [
+					"name" => $trigger,
+					"class" => $request,
+					"file" => $file,
+				],
+				"code" => 404,
 			];
 		}
-	} else {
-		$return = [
-			"error" => $API->Language->Field["no login info"],
-			"request" => $_POST,
-			"code" => 403,
-		];
+
+		if(!isset($return)){
+			// Maintenance Verification
+			if((!isset($API->Settings['maintenance']))||(!$API->Settings['maintenance'])){
+
+				// Login User
+				if(isset($_POST['method'])){
+					switch($_POST['method']){
+						case "token":
+							if(isset($_POST['token'])){ $API->Auth->loginToken($_POST['token']); }
+							else {
+								$return = [
+									"error" => $API->Language->Field["No Token Received"],
+									"request" => $_POST,
+									"api" => [
+										"name" => $trigger,
+										"class" => $request,
+										"file" => $file,
+									],
+									"code" => 403,
+								];
+							}
+							break;
+						case "auth":
+							if(isset($_POST['username'],$_POST['password'])){ $API->Auth->login($_POST['username'],$_POST['password']); }
+							else {
+								$return = [
+									"error" => $API->Language->Field["No Login Data Received"],
+									"request" => $_POST,
+									"api" => [
+										"name" => $trigger,
+										"class" => $request,
+										"file" => $file,
+									],
+									"api" => $trigger,
+									"code" => 403,
+								];
+							}
+							break;
+						case "session":
+							// In this case, user is logged in during API initialization
+							break;
+					}
+					if(!isset($return)){
+						if($API->Auth->isLogin()){
+
+							// Initialize Data
+							if(isset($_POST['data'])){
+								if($API->isJson($_POST['data'])){ $data = json_decode($decodedURI, true); }
+								else { $data = json_decode(urldecode(base64_decode($_POST['data'])), true); }
+							} else { $data = []; }
+
+							// Handling API Request
+							if(isset($_POST['type'])){
+								$method = $_POST['type'];
+								switch($method){
+									case"initialize":
+										if(method_exists($API,'initApp')){ $return = $API->initApp(); }
+										else {
+											$return = [
+												"error" => $API->Language->Field["Unknown Request"],
+												"request" => $_POST,
+												"api" => [
+													"name" => $trigger,
+													"class" => $request,
+													"file" => $file,
+												],
+												"code" => 404,
+											];
+										}
+										break;
+									case"getLanguage":
+										if(method_exists($API,$method)){ $return = $API->getLanguage(); }
+										else {
+											$return = [
+												"error" => $API->Language->Field["Unknown Request"],
+												"request" => $_POST,
+												"api" => [
+													"name" => $trigger,
+													"class" => $request,
+													"file" => $file,
+												],
+												"code" => 404,
+											];
+										}
+										break;
+									case"send":
+										if(property_exists($API,'Auth') && property_exists($API->Auth,'Mail') && method_exists($API->Auth->Mail,$method)){
+											if(isset($data['extra'])){$API->Auth->Mail->send($data['email'],$data['message'],$data['extra']);}
+											else{$API->Auth->Mail->send($data['email'],$data['message']);}
+											$return = ["success" => $API->Language->Field["Message Sent"]];
+										}
+										else {
+											$return = [
+												"error" => $API->Language->Field["Unknown Request"],
+												"request" => $_POST,
+												"api" => [
+													"name" => $trigger,
+													"class" => $request,
+													"file" => $file,
+												],
+												"code" => 404,
+											];
+										}
+										break;
+									default:
+										if(method_exists($API,$method)){
+											if($API->Auth->valid('api',$trigger,1)){
+												$return = $API->$method($trigger, $data);
+											} else {
+												$return = [
+													"error" => $API->Language->Field["Insuffisant Priviledges"],
+													"request" => $_POST,
+													"api" => [
+														"name" => $trigger,
+														"class" => $request,
+														"file" => $file,
+													],
+													"code" => 403,
+												];
+											}
+										}
+										else {
+											$return = [
+												"error" => $API->Language->Field["Unknown Request"],
+												"request" => $_POST,
+												"api" => [
+													"name" => $trigger,
+													"class" => $request,
+													"file" => $file,
+												],
+												"code" => 404,
+											];
+										}
+										break;
+								}
+							} else {
+								$return = [
+									"error" => $API->Language->Field["Unknown Request"],
+									"request" => $_POST,
+									"api" => [
+										"name" => $trigger,
+										"class" => $request,
+										"file" => $file,
+									],
+									"code" => 404,
+								];
+							}
+						} else {
+							$return = [
+								"error" => $API->Language->Field["Unable to Login User"],
+								"request" => $_POST,
+								"api" => [
+									"name" => $trigger,
+									"class" => $request,
+									"file" => $file,
+								],
+								"code" => 403,
+							];
+						}
+					}
+				} else {
+					$return = [
+						"error" => $API->Language->Field["Unknown Authentication Method"],
+						"request" => $_POST,
+						"api" => [
+							"name" => $trigger,
+							"class" => $request,
+							"file" => $file,
+						],
+						"code" => 403,
+					];
+				}
+			} else {
+				$return = [
+					"error" => $API->Language->Field["Server Under Maintenance"],
+					"request" => $_POST,
+					"api" => [
+						"name" => $trigger,
+						"class" => $request,
+						"file" => $file,
+					],
+					"code" => 500,
+				];
+			}
+		}
+		// Encoded JSON Response
+		echo base64_encode(urlencode(json_encode($return, JSON_PRETTY_PRINT)));
 	}
-} else {
-	$return = [
-		"error" => $API->Language->Field["no request received"],
-		"request" => $_POST,
-		"code" => 404,
-	];
 }
-if(!isset($return) || $return == "null" || $return == null || $return == ""){
-	$return = [
-		"error" => $API->Language->Field["nothing returned"],
-		"request" => $_POST,
-	];
-}
-echo base64_encode(urlencode(json_encode($return, JSON_PRETTY_PRINT)));
-?>
